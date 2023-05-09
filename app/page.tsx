525574d0e30778dee6a96906f46daacebf8923f4
ptnoire <ptnoire@gmail.com>
2023-05-09 10:59:44 -0400

    just centering

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 "use client";
 import { TranslateNumbers } from "@/components/translate-numbers";
-import Image from "next/image";
-import { useState } from "react";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center p-24 max-w-4xl mx-auto">
-      <h1 className="text-slate-100 text-3xl font-bold text-center">
+    <main className="flex min-h-screen justify-center flex-col items-center p-24 max-w-4xl mx-auto">
+      <h1 className="text-slate-100 text-5xl font-bold text-center">
         Bonjour le Monde!
       </h1>
       <TranslateNumbers />
